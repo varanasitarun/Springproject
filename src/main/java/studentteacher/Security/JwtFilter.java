8c2de6a1326a7cd94ea10da092856dc0fa2831c7
varanasitarun <tarunsaivaranasi1999@gmail.com>
2025-03-25 13:20:08 -0500

    removed unnecessary files

--- src/main/java/studentteacher/Security/JwtFilter.java
+++ src/main/java/studentteacher/Security/JwtFilter.java
@@ -35,6 +35,7 @@ public class JwtFilter extends OncePerRequestFilter {
         String token = null;
         String username = null;
 
+
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
             username = jwtService.extractUserName(token);
